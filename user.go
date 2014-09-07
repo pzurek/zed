8ba678779400f44e7259be2164ffc2617a3d9218
Piotr Zurek <p.zurek@gmail.com>
2014-09-07 16:27:10 +1200

    various fixes

--- user.go
+++ user.go
@@ -1,18 +1,16 @@
 package zd
 
 type User struct {
-	Id        *float64  `json:"id" gorethink:"id"`
-	Name      *string   `json:"name" gorethink:"name"`
-	Active    *bool     `json:"active" gorethink:"active"`
-	CreatedAt *string   `json:"created_at" gorethink:"created_at"`
-	UpdatedAt *string   `json:"updated_at" gorethink:"updated_at"`
-	Tags      *[]string `json:"tags" gorethink:"tags"`
-	// UserFields *map[string]interface{} `json:"user_fields" gorethink:"-"`
-	// Phone      bool                    `json:"-" gorethink:"phone"`
+	ID        *float64 `json:"id,omitempty"`
+	Name      *string  `json:"name,omitempty"`
+	Active    *bool    `json:"active,omitempty"`
+	CreatedAt *string  `json:"created_at,omitempty"`
+	UpdatedAt *string  `json:"updated_at,omitempty"`
+	Tags      []string `json:"tags,omitempty"`
 }
 
 type UsersResponse struct {
-	Users    *[]User `json:"users"`
+	Users    []User  `json:"users,omitempty"`
 	Next     *string `json:"next_page,omitempty"`
 	Previous *string `json:"previous_page,omitempty"`
 	Count    *int    `json:"count,omitempty"`
@@ -23,14 +21,14 @@ type UserService struct {
 }
 
 func (s *UserService) Get() ([]User, error) {
-	resource := make([]User, 0)
+	var resource []User
 
 	rp, next, _, err := s.getPage("")
 	if err != nil {
 		return nil, err
 	}
 
-	resource = append(resource, *rp...)
+	resource = append(resource, rp...)
 
 	for next != nil {
 		rp, nx, _, err := s.getPage(*next)
@@ -38,16 +36,16 @@ func (s *UserService) Get() ([]User, error) {
 			return nil, err
 		}
 		next = nx
-		resource = append(resource, *rp...)
+		resource = append(resource, rp...)
 	}
 
 	return resource, err
 }
 
-func (s *UserService) getPage(url string) (*[]User, *string, *Response, error) {
+func (s *UserService) getPage(url string) ([]User, *string, *Response, error) {
 
 	if url == "" {
-		url = "users.json?role=agent"
+		url = "users.json"
 	}
 
 	req, err := s.client.NewRequest("GET", url, nil)
