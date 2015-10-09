6cffc14de91c15b4014f041441742a896f3d024a
Piotr Zurek <p.zurek@gmail.com>
2015-10-09 19:28:56 +1300

    Update group.go

--- group.go
+++ group.go
@@ -4,12 +4,12 @@ package zd
 
 
 type Group struct {
-	URL       *string  `json:"url,omitempty"`
-	ID        *float64 `json:"id,omitempty"`
+	Url       *string  `json:"url,omitempty"`
+	Id *float64 `json:"id,omitempty"`
 	
 	
 	Name      *string  `json:"name,omitempty"`
-	Deleted   *bool    `json:"deleted,omitempty"`    
-	CreatedAt *string  `json:"created_at,omitempty"`
-	UpdatedAt *string  `json:"updated_at,omitempty"`
+	Deleted   *bool    `json:"deleted,omitempty"`      
+	CreatedAt *string  `json:"created_at,omitempty"`     
+	UpdatedAt *string  `json:"updated_at,omitempty"`     
 }
